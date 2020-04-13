0c5cd9181563677edcb6dbcd070e30a7407275e1
Bradydawg <iancrawford73@gmail.com>
2020-04-13 05:26:14 +0000

    Update src/main/java/com/bradydawg/hubcore/ChatColorCommand.java

--- src/main/java/com/bradydawg/hubcore/ChatColorCommand.java
+++ src/main/java/com/bradydawg/hubcore/ChatColorCommand.java
@@ -9,9 +9,10 @@ import org.bukkit.entity.Player;
 
 public class ChatColorCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!sender.hasPermission("ChatColor.GUI") || !(sender instanceof Player))
+        if(sender.hasPermission("chatcolor.gui"))
+            new FullMenu((Player)sender);
+        else if (sender.hasPermission("chatcolor.gui") == false)
             sender.sendMessage(ChatColor.RED + "You lack permission to use this command.");
-        new FullMenu((Player)sender);
         return true;
     }
 }
