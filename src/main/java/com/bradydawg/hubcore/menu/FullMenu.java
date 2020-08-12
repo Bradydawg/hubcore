c6445e2e757d48a8fc8cfe82cc1f5202bf6f0caf
Bradydawg <iancrawford73@gmail.com>
2020-08-12 18:25:43 -0400

    Added Creds

--- src/main/java/com/bradydawg/hubcore/menu/FullMenu.java
+++ src/main/java/com/bradydawg/hubcore/menu/FullMenu.java
@@ -1,3 +1,9 @@
+/*
+ * All rights by Bradydawg (2020)
+ * You are NOT allowed to modify this code unless you talk to Bradydawg beforehand
+ * You are NOT allowed to claim this plugin (HubCore) as your own
+ * You are NOT allowed to publish this plugin (HubCore) or your modified version of this plugin (HubCore)
+ */
 package com.bradydawg.hubcore.menu;
 
 import java.util.Arrays;
